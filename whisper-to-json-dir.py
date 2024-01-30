359d3463867e4776c10c8e7d13e14f5595fe7ecf
PiotrEsse <42764623+PiotrEsse@users.noreply.github.com>
2024-01-30 17:46:55 +0100

    Update whisper-to-json-dir.py
    small updates with comments

--- whisper-to-json-dir.py
+++ whisper-to-json-dir.py
@@ -2,7 +2,12 @@ import json
 import os
 import whisper_timestamped as whisper
 
+# List of available Whisper models
+# models = ["tiny", "base", "small", "medium", "large"] 
+
 model = whisper.load_model("tiny")
+
+# provide full path to your folder with audiofiles
 directory = "/home/piotr/Whisper/Audio"
 audio_formats = (".mp4",".mp3", ".wav", ".amr", ".aac", ".ogg", ".m4a")
 
@@ -12,4 +17,3 @@ for filename in os.listdir(directory):
         result = whisper.transcribe(model, audio, language="pl")
         with open(os.path.join(directory, filename.rsplit(".", 1)[0] + '.json'), 'w', encoding='utf-8') as f:
             json.dump(result, f, ensure_ascii=False, indent=2)
-
